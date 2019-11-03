7b2fbae4b6ece847f296180c5f21ba71dde600d5
njridsdale <njridsdale1@sheffield.ac.uk>
2019-11-03 10:17:34 +0000

    fix html not exist

--- cookiecrumbs/cookiecrumbs/settings.py
+++ cookiecrumbs/cookiecrumbs/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cookiecrumbs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['cookiecrumbsapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- cookiecrumbs/cookiecrumbsapp/views.py
+++ cookiecrumbs/cookiecrumbsapp/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cookieCrumbs(request):
-    return HttpResponse("cookieCrumbs")
+    return render(request, 'cookiecrumbsapp/cookieCrumbs.html')
 
 def getTrail(request):
     return HttpResponse("getTrail")
 
 def cookieTrail(request):
-    return HttpResponse("cookieTrail")
+    return render(request, 'cookiecrumbsapp/cookieTrail.html')
