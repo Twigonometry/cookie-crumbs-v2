6c9b4ce92ddbe7a34be1781ed133565f2ec67ba7
Twigonometry <mac.j.goodwin@gmail.com>
2019-11-03 10:33:12 +0000

    Cookie trail code for passing cookie objects to cookieTrail page

--- cookiecrumbs/cookiecrumbsapp/views.py
+++ cookiecrumbs/cookiecrumbsapp/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import PyChromeDevTools as pyChrome
+import http.cookies as cookies
+import json
 
 def cookieCrumbs(request):
     return render(request, 'cookiecrumbsapp/cookieCrumbs.html')
 
 def getTrail(request):
-    return HttpResponse("getTrail")
+    #setting up chrome dev tools interface
+    chrome = pyChrome.ChromeInterface()
+    chrome.Network.enable()
+    chrome.Page.enable()
+
+    #temporary call to google.com
+    chrome.Page.navigate(url="https://www.google.com/")
+    chrome.wait_event("Page.frameStoppedLoading",timeout=60)
+
+    #get cookies
+    cookies = chrome.Network.getCookies()
+
+    #pass cookies back to cookieTrail
+    newstr = " ".join(("http://127.0.0.1:8000/cookiecrumbsapp/cookieTrail?cookieList=", str(cookies)))
+    chrome.Page.navigate(url=newstr)
+
+    return HttpResponse("")
 
 def cookieTrail(request):
-    return render(request, 'cookiecrumbsapp/cookieTrail.html')
+    #get cookies from url header and turn into dictionary
+    cookieList = request.GET.get('cookieList')
+    cookieDump = eval(cookieList)
+
+    #parse cookies as dictionary entries and save into array
+    cookieObjects = []
+    for cookie in cookieDump['result']['cookies']:
+       cookieObjects.append(cookie)
+
+    return render(request, 'cookiecrumbsapp/cookieTrail.html', {'cookieObjects':cookieObjects})
